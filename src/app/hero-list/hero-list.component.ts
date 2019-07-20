3455428d44bcc9637583b2790cebaf6e871afb0c
mark <markvanharmelen@gmail.com>
2019-07-20 23:07:12 +0100

    add blank line, correct spelling

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -12,7 +12,7 @@ export class HeroListComponent implements OnInit {
 
   constructor() {
 
-    this.heroes[0] = new Hero('Environmentalist', 'Greta Thurnberg', 'Inspiring g' +
+    this.heroes[0] = new Hero('Environmentalist', 'Greta Thunberg', 'Inspiring g' +
       'lobal environmental activism', 'https://tinyurl.com/greta-t-pic');
     //this.heroes[1] = new Hero('Steeple Climber', 'Fred Dibnah', 'Bodger', 'https://tinyurl.com/dibnah-pic');
     this.heroes[1] = new Hero('Lightning', 'Ursain Bolt', 'Really fast, victory pose', 'https://tinyurl.com/ursain-pic');
